a865e7ae672c3107a17cb9a2409304be2dba2067
Jeremy King <jeremykingdev@gmail.com>
2026-01-21 14:17:11 -0500

    updated comments and print statements

--- src/autoload/scene_manager.gd
+++ src/autoload/scene_manager.gd
@@ -1,13 +1,16 @@
 extends Node
 ## Manages the scene tree during runtime. Handles swapping scenes, particularly menus and rooms during gameplay.
 
-var current_scene: Node = null ## The current scene being shown to the player
-
-# Internal variables
+var current_scene: Node = null ## The scene currently being shown to the player
 var _loading_in_progress: bool = false ## internal - used to block SceneManager from attempting to load two things at the same time
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	# Set the current scene to the initial child of the root node.
+	_update_current_scene()
+
+# Update the reference to the current scene.
+func _update_current_scene() -> void:
 	current_scene = get_tree().current_scene
 
 ## Swaps to the specified scene and unloads the specified scene.
@@ -21,7 +24,7 @@ func swap_scenes(scene_to_load: String, load_as_child_of: Node, scene_to_unload:
 	
 	# Check that the specified scene to load exists.
 	if not ResourceLoader.exists(scene_to_load, "PackedScene"):
-		push_warning("Requested scene '%s' does not exist at path.", scene_to_load)
+		push_warning("SceneManager: Requested scene '%s' does not exist at path." % scene_to_load)
 		return
 	
 	# Load the desired scene.
@@ -29,17 +32,19 @@ func swap_scenes(scene_to_load: String, load_as_child_of: Node, scene_to_unload:
 	
 	# Check that the scene loaded correctly.
 	if loaded_scene == null:
-		push_warning("Requested scene '%s' did not load properly.", scene_to_load)
+		push_warning("SceneManager: Requested scene '%s' did not load properly." % scene_to_load)
 		return
 	
 	# If no node was specified to load the scene as a child of, default to making it a child of the root node.
 	if load_as_child_of == null: load_as_child_of = get_tree().root
 	
 	# Add the newly loaded scene to the scene tree.
+	print("SceneManager: Loading scene '%s'" % loaded_scene)
 	load_as_child_of.add_child(loaded_scene)
 	
 	# Unload the scene that is no longer needed.
 	if scene_to_unload != null and scene_to_unload != get_tree().root:
+		print("SceneManager: Unloading scene '%s'" % scene_to_unload)
 		scene_to_unload.queue_free()
 		
 	# Reset loading in progress variable to indicate that loading has completed.
--- src/objects/door/door.gd
+++ src/objects/door/door.gd
@@ -1,16 +1,20 @@
 class_name Door extends Area2D
 ## A door that the player can interact with to move to a new room.
 
-## Signifies when a player has interacted with a door.
+## Triggered when the player interacts with this Door.
 signal player_entered_door(door: Door)
 
-## The scene to load when the player enters the door.
-@export var path_to_new_scene: String
+## The path of the Room to load when the player enters this Door.
+@export var path_to_target_room: String
 
-## The name of the door to enter through in the loaded scene.
-@export var entry_door_name: String
+## The name of this Door.
+@export var door_name: String
+
+## The name of the Door to spawn at in the target Room.
+@export var target_door_name: String
 
 ## Emit the player_entered_door signal when the door is entered.
-func _on_player_entered_door() -> void:
-	print("door interacted with. emitting player_entered_door signal")
+func _on_player_door_interact() -> void:
+	# Should only emit signal when player presses correct control.
+	print("door entered. emitting player_entered_door signal")
 	player_entered_door.emit(self)
