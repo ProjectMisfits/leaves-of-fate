65bdfc4a5ef0147c0498dbd23ff7ca0a94720255
kat24-4 <ktse2424@yahoo.com>
2026-01-23 08:53:49 -0500

    finished main menu base
    Everything should be in place and ready to hooked up to their
    corresponding things. In this commit, the leaf pointer is set to show up
    on focus (what I think the controller will do). There is code to have it
    work for hover in the file, but it is just commented out.

--- src/ui/leaf_pointer_animation.gd
+++ src/ui/leaf_pointer_animation.gd
@@ -5,13 +5,21 @@ extends Button
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	connect("mouse_entered", Callable(self, "_leaf_pointer_on"))
-	connect("mouse_exited", Callable(self, "_leaf_pointer_off"))
+	# Connect Hover (uncomment to activate)
+	#connect("mouse_entered", Callable(self, "_leaf_pointer_on"))
+	#connect("mouse_exited", Callable(self, "_leaf_pointer_off"))
 	
+	# Connect focus
+	connect("focus_entered", Callable(self, "_leaf_pointer_on"))
+	connect("focus_exited", Callable(self, "_leaf_pointer_off"))
+	
+	# Start hidden
 	my_leaf_pointer.hide()
 
+# Show leaf pointer
 func _leaf_pointer_on() -> void:
 	my_leaf_pointer.show()
 
+# Hide leaf pointer
 func _leaf_pointer_off() -> void:
 	my_leaf_pointer.hide()
