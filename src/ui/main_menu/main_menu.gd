20ab3564649b02a3c96ab079deaa06b7b1eec6b7
kat24-4 <ktse2424@yahoo.com>
2026-01-23 09:28:07 -0500

    attaching play button and updating all button hover to match

--- src/ui/main_menu/main_menu.gd
+++ src/ui/main_menu/main_menu.gd
@@ -6,5 +6,5 @@ func _ready() -> void:
 	pass # Replace with function body.
 
 # Signals that the game is started.
-func _on_button_button_up() -> void:
+func _on_play_button_button_up() -> void:
 	SceneManager.swap_scenes("res://src/gameplay/gameplay.tscn", null, self)
