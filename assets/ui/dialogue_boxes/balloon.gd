24e44182aea2b63238232a8930534b0157c1bb7d
JacobAntepli <70148209+JacobAntepli@users.noreply.github.com>
2026-01-26 10:41:35 -0500

    Set Up base dialogue and npc system

--- assets/ui/dialogue_boxes/balloon.gd
+++ assets/ui/dialogue_boxes/balloon.gd
@@ -211,3 +211,11 @@ func _on_responses_menu_response_selected(response: DialogueResponse) -> void:
 
 
 #endregion
+
+
+func _on_dialogue_label_spoke(letter: String, letter_index: int, speed: float) -> void:
+	#don't make sounds on space
+	if not letter in [" ", "."]:
+		audio_stream_player.pitch_scale = randf_range(.9,1.1)
+		audio_stream_player.play()
+	pass # Replace with function body.
