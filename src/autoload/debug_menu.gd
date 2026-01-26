16c38151bc73f263765c8e295b4091056e88afeb
Jeremy King <jeremykingdev@gmail.com>
2026-01-26 16:04:47 -0500

    added styling to debug menu for visibility
    added monospace font that shows up as white

--- src/autoload/debug_menu.gd
+++ src/autoload/debug_menu.gd
@@ -1,11 +1,11 @@
-extends Control
+extends CanvasLayer
 ## A debug menu for the game.
 
 ## An array that will hold any debug labels we create.
 var properties: Array
 
 ## A reference to the VBoxContainer the properties will exist in.
-@onready var container: VBoxContainer = $PanelContainer/VBoxContainer
+@onready var container: VBoxContainer = $PanelContainer/VBoxContainer/VBoxContainer
 
 ## The frame time. Used for variable update frequency.
 const fps_ms: int = 16
@@ -13,6 +13,10 @@ const fps_ms: int = 16
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	visible = false
+
+# Called once on each physics tick.
+func _physics_process(_delta: float) -> void:
+	self.add_debug_property("Seconds Elapsed", snapped(Time.get_ticks_msec() / 1000.0, 0.1), 10)
 
 ## Toggle the debug menu when the debug input is pressed.
 func _input(event: InputEvent) -> void:
@@ -32,5 +36,4 @@ func add_debug_property(id: StringName, value: Variant, time_in_frames: int) -> 
 		container.add_child(property)
 		property.name = id
 		property.text = id + ": " + str(value)
-		properties.append(property)
-	pass
+		properties.append(id)
