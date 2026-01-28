69cb75accdcdf6e2fc31032b2688aa29f4ae94ae
kat24-4 <ktse2424@yahoo.com>
2026-01-28 15:13:16 -0500

    added comments

--- src/ui/main_menu/main_menu.gd
+++ src/ui/main_menu/main_menu.gd
@@ -3,17 +3,20 @@ class_name MainMenu extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	$"CanvasLayer/MainSelectionsContainer/PlayButton".grab_focus.call_deferred()
+	$CanvasLayer/MainSelectionsContainer/PlayButton.grab_focus.call_deferred()
 
 # Signals that the game is started.
 func _on_play_button_button_up() -> void:
 	SceneManager.swap_scenes("res://src/gameplay/gameplay.tscn", null, self)
 
+# Open settings menu when corresponding button is pressed
 func _on_settings_button_up() -> void:
 	SceneManager.swap_scenes("res://src/ui/settings_menu/settings_menu.tscn", null, self)
 
+# Open controls menu when how to play is pressed
 func _on_return_label_button_up() -> void:
 	SceneManager.swap_scenes("res://src/ui/settings_menu/controls_menu/controls_menu.tscn", null, self)
 
+# Quit game when exit is pressed
 func _on_exit_button_up() -> void:
 	get_tree().quit()
