51a0792ff4260d6ebc49f91b66d5b268b538270b
kat24-4 <ktse2424@yahoo.com>
2026-01-28 15:39:28 -0500

    added canvas layer to center settings menu on screen

--- src/ui/settings_menu/settings_menu.gd
+++ src/ui/settings_menu/settings_menu.gd
@@ -3,7 +3,7 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	$"Panel/ControlsContainer/ControlsButton".grab_focus.call_deferred()
+	$"CanvasLayer/Panel/ControlsContainer/ControlsButton".grab_focus.call_deferred()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
