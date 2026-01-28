15379c23bf0510b412822c4c87dc54a99b9d6aa7
JacobAntepli <70148209+JacobAntepli@users.noreply.github.com>
2026-01-28 16:22:32 -0500

    Fixed all pull request items and switched talkables to interactables

--- src/actors/npc/talkables/talkable_area.gd
+++ src/actors/npc/talkables/talkable_area.gd
@@ -1,14 +1,8 @@
 extends Area2D
-
+class_name Talkable
 #Dialogue that this character is currently has
-@export var dialogue_resource: DialogueResource
-#Where the dialogue will start when you interact
-@export var dialogue_start: String = "start"
-#What balloon are they using 
-@export var balloon: PackedScene = null
 
-func talk() -> void:
-	print("Attempting speech")
-	DialogueManager.show_dialogue_balloon_scene(balloon,dialogue_resource,dialogue_start)
+
+	
 
 	
