8d60a5311dc14134310e0dfe16641ef82ee1cfd6
kat24-4 <ktse2424@yahoo.com>
2026-01-28 13:25:00 -0500

    attached setting and how to play

--- src/ui/main_menu/main_menu.gd
+++ src/ui/main_menu/main_menu.gd
@@ -8,3 +8,9 @@ func _ready() -> void:
 # Signals that the game is started.
 func _on_play_button_button_up() -> void:
 	SceneManager.swap_scenes("res://src/gameplay/gameplay.tscn", null, self)
+
+func _on_settings_button_up() -> void:
+	SceneManager.swap_scenes("res://src/ui/settings_menu/settings_menu.tscn", null, self)
+	
+func _on_return_label_button_up() -> void:
+	SceneManager.swap_scenes("res://src/ui/settings_menu/controls_menu/controls_menu.tscn", null, self)
