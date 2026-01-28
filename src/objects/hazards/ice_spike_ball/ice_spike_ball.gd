375ea1db8bd9921eaf61695b7229154cfb77c977
Jeremy King <jeremykingdev@gmail.com>
2026-01-28 12:18:06 -0500

    created independent icicle and ice spike ball entities

--- src/objects/hazards/ice_spike_ball/ice_spike_ball.gd
+++ src/objects/hazards/ice_spike_ball/ice_spike_ball.gd
@@ -1,8 +1,8 @@
-class_name Hazard extends StaticBody2D
-## An entity that hurts the player when they hit it.
+class_name IceSpikeBall extends Area2D
+## A big ice spike ball that hurts the player when they hit it.
 
 ## Triggers when the player enters the area of the obstacle.
-func _on_area_2d_body_entered(body: Node2D) -> void:
+func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group('player'):
 		print("Hazard: The player hit '%s'" % self.name)
 	pass # Replace with function body.
