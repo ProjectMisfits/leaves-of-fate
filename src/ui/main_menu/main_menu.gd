aeed649f6b9c49b72235fdc8e523fda8374870ee
kat24-4 <ktse2424@yahoo.com>
2026-01-28 14:20:52 -0500

    added canvas layer to pause menu to keep it in the right place

--- src/ui/main_menu/main_menu.gd
+++ src/ui/main_menu/main_menu.gd
@@ -11,9 +11,9 @@ func _on_play_button_button_up() -> void:
 
 func _on_settings_button_up() -> void:
 	SceneManager.swap_scenes("res://src/ui/settings_menu/settings_menu.tscn", null, self)
-	
+
 func _on_return_label_button_up() -> void:
 	SceneManager.swap_scenes("res://src/ui/settings_menu/controls_menu/controls_menu.tscn", null, self)
-	
+
 func _on_exit_button_up() -> void:
 	get_tree().quit()
