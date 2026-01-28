43cb87b7c0f982ae6d3605931ff6908074f5a7fa
Jeremy King <jeremykingdev@gmail.com>
2026-01-28 12:28:11 -0500

    add hurting player to icicle and ice spike ball scripts

--- src/objects/hazards/ice_spike_ball/ice_spike_ball.gd
+++ src/objects/hazards/ice_spike_ball/ice_spike_ball.gd
@@ -3,6 +3,7 @@ class_name IceSpikeBall extends Area2D
 
 ## Triggers when the player enters the area of the obstacle.
 func _on_body_entered(body: Node2D) -> void:
-	if body.is_in_group('player'):
+	if body is Player:
+		body.hurt(1)
 		print("Hazard: The player hit '%s'" % self.name)
 	pass # Replace with function body.
--- src/objects/hazards/icicle/icicle.gd
+++ src/objects/hazards/icicle/icicle.gd
@@ -3,6 +3,7 @@ class_name Icicle extends StaticBody2D
 
 ## Triggers when the player enters the area of the obstacle.
 func _on_area_2d_body_entered(body: Node2D) -> void:
-	if body.is_in_group('player'):
+	if body is Player:
+		body.hurt(1)
 		print("Hazard: The player hit '%s'" % self.name)
 	pass # Replace with function body.
