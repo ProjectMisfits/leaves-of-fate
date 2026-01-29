ca9c7993cd0243579e993e4de599ae0d40ce843e
kat24-4 <ktse2424@yahoo.com>
2026-01-29 11:35:57 -0500

    added intro letter and hooked it up to main menu and gameplay

--- src/ui/main_menu/main_menu.gd
+++ src/ui/main_menu/main_menu.gd
@@ -10,9 +10,10 @@ func _process(delta: float) -> void:
 
 # Signals that the game is started.
 func _on_play_button_button_up() -> void:
-	SceneManager.swap_scenes("res://src/gameplay/gameplay.tscn", null, self)
+	SceneManager.swap_scenes("res://src/ui/intro_letter/intro_letter.tscn", null, self)
 
 func _hide_main_menu() -> void:
+	
 	$CanvasLayer/EnvelopeBase.hide()
 	$CanvasLayer/MainSelectionsContainer.hide()
 
