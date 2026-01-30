287472ec83de629376e055683d00dd8754d19d03
JacobAntepli <70148209+JacobAntepli@users.noreply.github.com>
2026-01-30 14:20:50 -0500

    Implemented base animations for player

--- src/actors/characters/player/states/player_piling.gd
+++ src/actors/characters/player/states/player_piling.gd
@@ -2,7 +2,8 @@ extends LimboState
 
 func _enter() -> void:
 	#print("Player State Transition: to_pileing")
-	agent.animated_sprite_2d.animation = &"pile"
+	agent.animation_player.play("player_leaf_dash")
+	
 	agent.collision_shape_2d.shape = agent.collision_dash
 	agent.velocity.y = 0.0
 
@@ -22,5 +23,5 @@ func _update(delta: float) -> void:
 		agent.move_horizontal_pile_air()
 
 func _exit() -> void:
-	agent.animated_sprite_2d.animation = &"player"
+
 	agent.collision_shape_2d.shape = agent.collision_normal
--- src/actors/characters/player/states/player_running.gd
+++ src/actors/characters/player/states/player_running.gd
@@ -2,6 +2,7 @@ extends LimboState
 
 func _enter() -> void:
 	#print("Player State Transition: to_running")
+	agent.animation_player.play("player_walk_cycle")
 	pass
 
 func _update(_delta: float) -> void:
