368ed417911f2d05eefacb6f78af1fc326da06df
Jeremy King <jeremykingdev@gmail.com>
2026-01-30 16:34:30 -0500

    fixed incorrect player preload path in gameplay

--- src/gameplay/gameplay.gd
+++ src/gameplay/gameplay.gd
@@ -9,7 +9,7 @@ class_name Gameplay extends Node2D
 var current_room: Room = null
 
 ## The player object.
-var player: Player = preload("res://src/actors/characters/player/player.tscn").instantiate()
+var player: Player = preload("res://src/entities/actors/characters/player/player.tscn").instantiate()
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
