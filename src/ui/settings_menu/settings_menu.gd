facbadaa52fd708bcb07b770e69e79e257c87e39
kat24-4 <ktse2424@yahoo.com>
2026-02-02 09:03:39 -0500

    remove process function from settings

--- src/ui/settings_menu/settings_menu.gd
+++ src/ui/settings_menu/settings_menu.gd
@@ -3,8 +3,3 @@ extends Control
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	get_node("%ControlsButton").grab_focus.call_deferred()
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
