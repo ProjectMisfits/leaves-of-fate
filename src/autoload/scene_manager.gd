b656d58ede75a23db440822a18889cb913a36858
Jeremy King <jeremykingdev@gmail.com>
2026-02-09 15:24:31 -0500

    added fade to black screen transition

--- src/autoload/scene_manager.gd
+++ src/autoload/scene_manager.gd
@@ -4,6 +4,15 @@ extends Node
 ## The current first child scene of the tree root.
 var current_scene: Node = null
 
+## Wether scenes are currently being swapped out
+var swap_in_progress: bool = false
+
+## Reference to screen transition PackedScene
+var screen_transition_scene: PackedScene = preload("res://src/ui/screen_transition/screen_transition.tscn")
+
+## A reference to the current screen transition. If there are no active screen transitions, this variable is null.
+var current_screen_transition: ScreenTransition = null
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	current_scene = get_tree().current_scene
@@ -20,12 +29,22 @@ func swap_scenes(scene_to_load: String, load_as_child_of: Node, scene_to_unload:
 		push_warning("SceneManager: Requested scene '%s' does not exist at path." % scene_to_load)
 		return -1
 	
+	if swap_in_progress:
+		push_warning("SceneManager: A scene is already being loaded!")
+		return -1
+	
+	swap_in_progress = true
+	
+	# Start the screen transition.
+	_add_screen_transition()
+	
 	# Load the desired scene.
 	var loaded_scene: Node = ResourceLoader.load(scene_to_load, "PackedScene").instantiate()
 	
 	# Check that the scene loaded correctly.
 	if loaded_scene == null:
 		push_warning("SceneManager: Requested scene '%s' did not load properly." % scene_to_load)
+		swap_in_progress = false
 		return -1
 	
 	# If no node was specified to load the scene as a child of, default to making it a child of the root node.
@@ -43,4 +62,26 @@ func swap_scenes(scene_to_load: String, load_as_child_of: Node, scene_to_unload:
 	# Update the current scene if the swap was made directly under the root node.
 	if load_as_child_of == get_tree().root:
 		current_scene = loaded_scene
+	
+	_remove_screen_transition()
+	
+	swap_in_progress = false
 	return 0
+
+## Create a screen transition, add it to the scene tree, and initiate the animation.
+func _add_screen_transition(transition_type: String = "fade_to_black") -> void:
+	current_screen_transition = screen_transition_scene.instantiate()
+	get_tree().root.add_child(current_screen_transition)
+	current_screen_transition.start_transition(transition_type)
+	# Wait for the animation to finish.
+	await current_screen_transition.transition_animation_player.animation_finished
+	
+## Reverse the screen transition animation and remove the screen transition from the scene tree.
+func _remove_screen_transition() -> void:
+	# Reverse the screen transition animation.
+	current_screen_transition.finish_transition()
+	# Wait for the animation to finish.
+	await current_screen_transition.transition_animation_player.animation_finished
+	# Remove and reset the current screen transition.
+	current_screen_transition.queue_free()
+	current_screen_transition = null
