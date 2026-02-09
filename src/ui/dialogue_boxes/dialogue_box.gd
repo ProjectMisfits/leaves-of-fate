9ec88ef0a6d036da4da6f34e2e88d9c5f62bbf75
JacobAntepli <70148209+JacobAntepli@users.noreply.github.com>
2026-02-09 13:37:19 -0500

    Made it so pressing the interact action skips typing

--- src/ui/dialogue_boxes/dialogue_box.gd
+++ src/ui/dialogue_boxes/dialogue_box.gd
@@ -94,11 +94,9 @@ func _ready() -> void:
 
 ##Function to set the interupt delay and set the variable to be true
 func interrupt(delay:String) -> void:
-	
 	interupt_delay = delay.to_float()
-	print(interupt_delay)
 	do_interupt = true
-	pass
+	
 
 func _process(_delta: float) -> void:
 	if is_instance_valid(dialogue_line):
@@ -229,10 +227,12 @@ func _on_mutated(_mutation: Dictionary) -> void:
 
 
 func _on_balloon_gui_input(event: InputEvent) -> void:
+
+	
 	# See if we need to skip typing of the dialogue
 	if dialogue_label.is_typing:
 		var mouse_was_clicked: bool = event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.is_pressed()
-		var skip_button_was_pressed: bool = event.is_action_pressed(skip_action)
+		var skip_button_was_pressed: bool = event.is_action_pressed("interact")
 		if mouse_was_clicked or skip_button_was_pressed:
 			get_viewport().set_input_as_handled()
 			dialogue_label.skip_typing()
@@ -248,7 +248,7 @@ func _on_balloon_gui_input(event: InputEvent) -> void:
 		do_interupt = false
 		next(dialogue_line.next_id)
 		
-	elif event.is_action_pressed(next_action) and get_viewport().gui_get_focus_owner() == balloon:
+	elif event.is_action_pressed("interact") and get_viewport().gui_get_focus_owner() == balloon:
 		interact_audio_player.play()
 		do_interupt = false
 		next(dialogue_line.next_id)
