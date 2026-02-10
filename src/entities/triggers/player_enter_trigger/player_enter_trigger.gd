cff948468614489d5a72f523eeeadd70258dfb05
Jeremy King <jeremykingdev@gmail.com>
2026-02-10 17:36:23 -0500

    fixed up incorrectly set top level variables in gameplay

--- src/entities/triggers/player_enter_trigger/player_enter_trigger.gd
+++ src/entities/triggers/player_enter_trigger/player_enter_trigger.gd
@@ -1,7 +1,6 @@
 @abstract class_name PlayerEnterTrigger extends Trigger
 ## Abstract base class for triggers that activate on player entry.
-var player : Player = null
-
+var player: Player = null
 
 ## Activate the trigger when the player enters the trigger area.
 func _on_body_entered(_body: Node2D) -> void:
