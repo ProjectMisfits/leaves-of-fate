dd873df3d1588362e8b2ade446cfd24f9ce60031
Jeremy King <jeremykingdev@gmail.com>
2026-02-10 11:30:59 -0500

    added script for npc movement scripting in cutscenes

---
+++ src/entities/actors/npcs/npc_base/npc_base.gd
@@ -0,0 +1,26 @@
+class_name NPC extends CharacterBody2D
+## A character body whose movement can be scripted during cutscenes.
+
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction := Input.get_axis("ui_left", "ui_right")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	move_and_slide()
+
+
+# Move (direction, duration, distance)
+# Disappear when offscreen
+# Face direction
