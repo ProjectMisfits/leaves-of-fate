db3ec039d2e5663b4f6ba09de95b8ae5de664d8f
Jeremy King <jeremykingdev@gmail.com>
2026-02-10 12:14:56 -0500

    added comments to ambiance resource

--- assets/ambiance/resources/ambiance.gd
+++ assets/ambiance/resources/ambiance.gd
@@ -1,4 +1,8 @@
 class_name Ambiance extends Resource
+## A resource for anbiance sounds.
 
+## The background audio for the ambiance sounds.
 @export var background : AudioStream
+
+## An array of one off sounds to play randomly when this ambiance is playing.
 @export var one_offs : Array[AudioStream]
