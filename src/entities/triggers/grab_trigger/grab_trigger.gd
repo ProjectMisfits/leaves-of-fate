381a533865d4d6c1ba8b956faa3fa41e64575d86
Jeremy King <jeremykingdev@gmail.com>
2026-02-11 12:06:12 -0500

    created grab animation for grab trigger

--- src/entities/triggers/grab_trigger/grab_trigger.gd
+++ src/entities/triggers/grab_trigger/grab_trigger.gd
@@ -7,6 +7,9 @@ var is_grabbed: bool = false
 ## A reference to the grab highlight.
 @onready var grab_highlight: Control
 
+## A reference to the grab trigger animation player.
+@onready var grab_animation_player: AnimationPlayer = $AnimationPlayer
+
 func _ready() -> void:
 	grab_highlight = $GrabHighlight
 	_on_trigger = _on_grab
@@ -14,12 +17,13 @@ func _ready() -> void:
 ## Grab the entity that this trigger is a child of.
 func _on_grab() -> void:
 	# If the parent has the grab handling functions,
-	# Show the grab highlight
-	# Call the grab handling functions
+	# Play the Az grab animation and call the grab handling functions
 	if get_parent().has_method("_ungrab") and is_grabbed:
+		grab_animation_player.play_backwards("grab")
 		get_parent()._ungrab()
 		is_grabbed = false
 	elif get_parent().has_method("_grab") and not is_grabbed:
+		grab_animation_player.play("grab")
 		get_parent()._grab()
 		is_grabbed = true
 	else:
