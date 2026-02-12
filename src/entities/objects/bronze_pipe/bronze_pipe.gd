0c77f39d3c2436e67bc48b46bc05dd29615cdb1a
Jeremy King <jeremykingdev@gmail.com>
2026-02-12 09:21:37 -0500

    created basic bronze pipe entity and script

---
+++ src/entities/objects/bronze_pipe/bronze_pipe.gd
@@ -0,0 +1,3 @@
+class_name BronzePipe extends Node2D
+## A bronze pipe that the player can only enter when in leaf mode. When entered, moves the player along the path of the pipe until they reach the exit.
+## When in a bronze pipe, the player has no input control.
