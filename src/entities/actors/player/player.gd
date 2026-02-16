9c39365f3ce7ba148beb6356bbf915e8f28aac0e
Jeremy King <jeremykingdev@gmail.com>
2026-02-16 14:32:20 -0500

    added basic function to player that resets velocity on cutscene triggered

--- src/entities/actors/player/player.gd
+++ src/entities/actors/player/player.gd
@@ -588,3 +588,8 @@ func add_debug_parameters() -> void:
 	DebugMenu.add_debug_property("Player Velocity", velocity, 5)
 	DebugMenu.add_debug_property("Jump Velocity",jump_velocity,0)
 	DebugMenu.add_debug_property("Jump Gravity",jump_gravity,0)
+
+## Enable cutscene mode for the player.
+func enable_cutscene_mode() -> void:
+	disable_player_input()
+	velocity = Vector2(0.0, 0.0)
