a96099bee1f692faec315b8befb26c1bee188c55
JacobAntepli <70148209+JacobAntepli@users.noreply.github.com>
2026-02-16 14:40:20 -0500

    prettied up code

--- src/entities/actors/enemies/ice_cube/ice_scream.gd
+++ src/entities/actors/enemies/ice_cube/ice_scream.gd
@@ -24,9 +24,6 @@ var gravity: int
 #The range at which the ice cube needs to get near the last know position of the player
 var end_range : int
 
-
-
-
 func _enter_tree() -> void:
 	if (database != null):
 		var db_data: Dictionary = database.data
@@ -59,7 +56,6 @@ func _physics_process(delta: float) -> void:
 	velocity.y += gravity * delta
 	move_and_slide()
 
-
 #Checking if the cube has reached the player
 func check_reached_player() -> void:
 	#Make sure that the player is not still visible even if you have reached the destination
@@ -70,23 +66,17 @@ func check_reached_player() -> void:
 		elif abs(global_position.x) - abs(player_last_known_pos.x) < end_range and  look_direction == -1:
 			state_machine.dispatch(&"to_idle")
 
-	
-
 #function to reset all of the cubes properties
 func reset() -> void:
-
 	ice_cube_sprite.modulate = Color(1.0, 1.0, 1.0, 1.0)
 	velocity.x = 0
 	player_last_known_pos = Vector2.ZERO
-
-	
 
 func move_normal(delta:float) -> void:
 	move_horizontal(look_direction,normal_acceleration,normal_deceleration,delta,turn_speed,max_speed)
 
 func move_idle(delta:float) -> void:
 	move_horizontal(0,0,normal_deceleration,delta,turn_speed,max_speed)
-
 
 #Checking if the ice cube has hit a wall, this stops an angry charge
 func check_wall() -> void:
@@ -102,8 +92,6 @@ func check_release(delay: bool) -> void:
 		else:
 			state_machine.dispatch(&"to_idle")
 
-	
-	
 #If the ice cube collides with another ice cube or spike at ANY state it should explode
 func _on_hurt_area_body_entered(body: Node2D) -> void:
 	
@@ -116,9 +104,6 @@ func _on_hurt_area_body_entered(body: Node2D) -> void:
 	elif(not body == self and (body is Icicle or body is IceSpikeBall) ):
 		
 		state_machine.dispatch(&"to_death")
-	
-	
-
 
 func _on_sight_body_entered(body: Node2D) -> void:
 	if(body.global_position.x-global_position.x < 0) and look_direction == 1:
