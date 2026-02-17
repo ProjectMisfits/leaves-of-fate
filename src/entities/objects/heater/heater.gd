c5b807927eeb7f77f92a471eaa3849c9b6513194
Jeremy King <jeremykingdev@gmail.com>
2026-02-17 13:20:15 -0500

    added basic logic and animation player to heater entity

--- src/entities/objects/heater/heater.gd
+++ src/entities/objects/heater/heater.gd
@@ -1,2 +1,16 @@
 class_name Heater extends Decor
-## A space heater that the player enables by interacting with a lever or something in the world.
+## A space heater that the player enables by interacting with something in the world.
+
+## Whether this heater is off.
+var heater_off: bool = true
+
+func _ready() -> void:
+	super()
+	EventFlags.flag_updated.connect(_on_event_flag_updated)
+
+## Triggered when an event flag is updated. If the flag indicates that a heater should change state, change the state of this heater to match.
+func _on_event_flag_updated(flag_name: String, value: bool) -> void:
+	if "heater" in flag_name and value and heater_off:
+		$AnimationPlayer.play(&"heater_on")
+		heater_off = false
+		print("working")
