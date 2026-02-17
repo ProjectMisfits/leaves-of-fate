dacaf1ea76f2a92b5e97cb14fcd8f39ffd7bb60b
JacobAntepli <70148209+JacobAntepli@users.noreply.github.com>
2026-02-17 17:55:23 -0500

    Made hurt component for player

--- src/entities/actors/player/components/hurt_component.gd
+++ src/entities/actors/player/components/hurt_component.gd
@@ -6,6 +6,12 @@ class_name HurtComponent
 ##A reference to the hurt areas collision box shape
 @onready var hurt_box : CollisionShape2D = $HurtArea/CollisionShape2D
 
+##Reference to player
+@onready var player : Player = $"../.."
+
 
 func _ready() -> void:
 	hurt_box.shape = player_collision.shape
+
+func _on_hurt_area_body_entered(body: Node2D) -> void:
+	player.hurt(1)
