3c702c0ab82be6cd711ca1db2736b55cbaee1bfd
JacobAntepli <70148209+JacobAntepli@users.noreply.github.com>
2026-02-17 17:44:39 -0500

    Started making spikes a tile map layer

---
+++ src/entities/actors/player/components/hurt_component.gd
@@ -0,0 +1,11 @@
+extends Node
+class_name HurtComponent
+##A reference to the players collision box shape
+@onready var player_collision : CollisionShape2D = $"../../CollisionShape2D"
+
+##A reference to the hurt areas collision box shape
+@onready var hurt_box : CollisionShape2D = $HurtArea/CollisionShape2D
+
+
+func _ready() -> void:
+	hurt_box.shape = player_collision.shape
