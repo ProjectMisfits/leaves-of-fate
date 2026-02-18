3784bb0bc79cfed878415e8b2ce30782c07bc86b
Jeremy King <jeremykingdev@gmail.com>
2026-02-18 11:26:03 -0500

    fixed music trigger fadeout bug

--- src/autoload/audio_managers/music_manager.gd
+++ src/autoload/audio_managers/music_manager.gd
@@ -2,34 +2,28 @@ extends AudioStreamPlayer
 ## A manager for playing music during runtime.
 
 ## Play the given music stream at the specified volume.
-## If the given music is already playing, it will just continue playing.
-## If the given music is not already playing, it will stop the currently playing music and play the new one.
+## If the given music is already playing, do nothing.
+## If the given music is not already playing, crossfade to the given music.
 func _play_song(music: AudioStream, volume: float = 0.0) -> void:
-	print(stream)
-	print(music)
-	# If the provided music is what's already playing, continue playing
+	# If the provided music is already playing, do nothing
 	if stream == music:
-		print("Same music")
-		if not playing:
-			play()
 		return
-
-	elif playing:
-		print("Crossfade")
-		crossfade(music, volume)
-
+	
+	if not playing:
+		# If nothing is playing, start the music directly without a fade.
+		stream = music
+		play()
 	else:
-		print("No music --> music")
-		#Plays the specified music
+		# Otherwise crossfade to the new music.
+		# If something is already playing, fade it out.
+		await _fade_out()
+		stop()
 		stream = music
 		volume_db = volume
 		play()
 
-
-func crossfade(music: AudioStream, volume: float = 0.0) -> void:
-	var tween : Tween = create_tween()
-	tween.tween_property(self,"volume_db",-60.0,0.5)
-	#await tween.finished
-	stream = music
-	play()
-	tween.tween_property(self,"volume_db",volume,0.5)
+## Fade out the currently playing music.
+func _fade_out() -> void:
+	var fade_out_tween: Tween = create_tween()
+	fade_out_tween.tween_property(self, "volume_db", -60.0, 0.5)
+	await fade_out_tween.finished
