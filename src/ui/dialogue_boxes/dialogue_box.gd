fcfa0e0c65688c3df5100bad0a42de5b12f2b8bb
Jeremy King <jeremykingdev@gmail.com>
2026-02-18 18:06:45 -0500

    changed dialogue progression button to ui_accept

--- src/ui/dialogue_boxes/dialogue_box.gd
+++ src/ui/dialogue_boxes/dialogue_box.gd
@@ -348,7 +348,7 @@ func _on_balloon_gui_input(event: InputEvent) -> void:
 	# See if we need to skip typing of the dialogue
 	if dialogue_label.is_typing:
 		var mouse_was_clicked: bool = event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.is_pressed()
-		var skip_button_was_pressed: bool = event.is_action_pressed("interact")
+		var skip_button_was_pressed: bool = event.is_action_pressed("ui_accept")
 		if mouse_was_clicked or skip_button_was_pressed:
 			get_viewport().set_input_as_handled()
 			dialogue_label.skip_typing()
@@ -364,7 +364,7 @@ func _on_balloon_gui_input(event: InputEvent) -> void:
 		do_interrupt = false
 		next(dialogue_line.next_id)
 		
-	elif event.is_action_pressed("interact") and get_viewport().gui_get_focus_owner() == balloon:
+	elif event.is_action_pressed("ui_accept") and get_viewport().gui_get_focus_owner() == balloon:
 		interact_audio_player.play()
 		do_interrupt = false
 		next(dialogue_line.next_id)
