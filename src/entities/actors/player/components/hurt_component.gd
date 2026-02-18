fae03a6a42d354177866fbeb3f1d7c27e025a8f5
Jeremy King <jeremykingdev@gmail.com>
2026-02-18 16:39:54 -0500

    fixed issue with hurt component not being monitorable

--- src/entities/actors/player/components/hurt_component.gd
+++ src/entities/actors/player/components/hurt_component.gd
@@ -1,12 +1,13 @@
 class_name HurtComponent extends Node2D
-## A component that monitors for its parent getting hurt.
+## A component that allows an entity to be hurt.
 
-## A signal to indicate this component has registered a hurt.
+## A signal to indicate this component has been hurt.
 signal hurt
 
 ## Whether the hurt has already occurred. Used to prevent repeat hurt triggers.
 var is_hurt: bool = false
 
+## Triggered when a hazard or enemy enters the hurt area.
 func _on_hurt_area_body_entered(_body: Node2D) -> void:
 	if not is_hurt:
 		hurt.emit()
