75890462a7a13abff965e014dc38d763cbb69626
cjpugs <cjcosta04@gmail.com>
2026-02-18 10:49:41 -0500

    trying to debug

--- src/autoload/audio_managers/music_manager.gd
+++ src/autoload/audio_managers/music_manager.gd
@@ -5,16 +5,21 @@ extends AudioStreamPlayer
 ## If the given music is already playing, it will just continue playing.
 ## If the given music is not already playing, it will stop the currently playing music and play the new one.
 func _play_song(music: AudioStream, volume: float = 0.0) -> void:
+	print(stream)
+	print(music)
 	# If the provided music is what's already playing, continue playing
 	if stream == music:
+		print("Same music")
 		if not playing:
 			play()
 		return
 
 	elif playing:
+		print("Crossfade")
 		crossfade(music, volume)
 
 	else:
+		print("No music --> music")
 		#Plays the specified music
 		stream = music
 		volume_db = volume
@@ -23,7 +28,8 @@ func _play_song(music: AudioStream, volume: float = 0.0) -> void:
 
 func crossfade(music: AudioStream, volume: float = 0.0) -> void:
 	var tween : Tween = create_tween()
-	tween.tween_property(self,"volume_db", -60.0, 2)
+	tween.tween_property(self,"volume_db",-60.0,0.5)
+	#await tween.finished
 	stream = music
 	play()
-	tween.tween_property(self,"volume_db",volume,1)
+	tween.tween_property(self,"volume_db",volume,0.5)
