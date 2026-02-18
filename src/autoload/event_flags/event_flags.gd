216004370504d7ecccef28af2059585eb5286639
cjpugs <cjcosta04@gmail.com>
2026-02-18 16:57:54 -0500

    removed winston name from event flags

--- src/autoload/event_flags/event_flags.gd
+++ src/autoload/event_flags/event_flags.gd
@@ -9,7 +9,6 @@ var _event_flags: Dictionary[String, bool]
 
 # EVENT FLAG-RELATED VARIABLES #
 var num_heaters_activated: int = 0	## Updates when a heater_activated flag is set
-var winston_name: String = "Wizard"	##  Updated by Dialogue resources during specific conversations
 
 func _enter_tree() -> void:
 	# Grab database programatically
