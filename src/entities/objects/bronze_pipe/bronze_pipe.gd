372be821b8614f7367b631b64c767963250fb1e1
Jeremy King <jeremykingdev@gmail.com>
2026-02-19 17:01:28 -0500

    refactored bronze pipes to use a local variable for the pipe path curve

--- src/entities/objects/bronze_pipe/bronze_pipe.gd
+++ src/entities/objects/bronze_pipe/bronze_pipe.gd
@@ -2,6 +2,9 @@ class_name BronzePipe extends Node2D
 ## A bronze pipe that the player can only enter when in leaf mode. When entered, moves the player along the path of the pipe until they reach the exit.
 ## When in a bronze pipe, the player has no input control.
 
+## A reference to the pipe path curve.
+@onready var _pipe_path_curve: Curve2D = $Path2D.curve
+
 ## A reference to the PathFollow2D node that will follow the pipe's path.
 @onready var _pipe_path_follower: PathFollow2D = %PathFollow2D
 
@@ -22,19 +25,19 @@ var _player: Player = null
 
 func _ready() -> void:
 	# Set up the pipe texture along the pipe's path.
-	for idx: int in $Path2D.curve.point_count:
-		$Path2D/Line2D.add_point($Path2D.curve.get_point_position(idx))
+	for idx: int in _pipe_path_curve.point_count:
+		$Path2D/Line2D.add_point(_pipe_path_curve.get_point_position(idx))
 	# Put the cap textures at the start and end of the pipe.
 	var start_cap_sprite: Sprite2D = Sprite2D.new()
 	start_cap_sprite.texture = preload("res://assets/entities/objects/bronze_pipe/PipeStraight-01-capstart.PNG")
-	start_cap_sprite.position = $Path2D.curve.get_point_position(0)
-	start_cap_sprite.rotate($Path2D.curve.get_point_position(0).angle_to($Path2D.curve.get_point_position(1)))
+	start_cap_sprite.position = _pipe_path_curve.get_point_position(0)
+	start_cap_sprite.rotate(_pipe_path_curve.get_point_position(0).angle_to(_pipe_path_curve.get_point_position(1)))
 	start_cap_sprite.scale = Vector2(1.5, 1.5)
 	add_child(start_cap_sprite)
 	var end_cap_sprite: Sprite2D = Sprite2D.new()
 	end_cap_sprite.texture = preload("res://assets/entities/objects/bronze_pipe/PipeStraight-01-capend.PNG")
-	end_cap_sprite.position = $Path2D.curve.get_point_position($Path2D.curve.point_count - 1)
-	end_cap_sprite.rotate($Path2D.curve.get_point_position($Path2D.curve.point_count - 2).angle_to_point($Path2D.curve.get_point_position($Path2D.curve.point_count - 1)))
+	end_cap_sprite.position = _pipe_path_curve.get_point_position(_pipe_path_curve.point_count - 1)
+	end_cap_sprite.rotate(_pipe_path_curve.get_point_position(_pipe_path_curve.point_count - 2).angle_to_point(_pipe_path_curve.get_point_position(_pipe_path_curve.point_count - 1)))
 	end_cap_sprite.scale = Vector2(1.5, 1.5)
 	add_child(end_cap_sprite)
 
@@ -81,7 +84,7 @@ func _exit_pipe() -> void:
 	_pipe_path_visual.hide()
 	# Show animation or particle visual of exiting pipe
 	# Reset player velocity so it launches out of the pipe instead of wonkily at the ground due to gravity
-	var new_player_velocity: Vector2 = Vector2.RIGHT.rotated($Path2D.curve.get_point_position($Path2D.curve.point_count - 2).angle_to_point($Path2D.curve.get_point_position($Path2D.curve.point_count - 1))) * _player.dash_end_velocity_multiplier * $Path2D.curve.get_baked_length()
+	var new_player_velocity: Vector2 = Vector2.RIGHT.rotated(_pipe_path_curve.get_point_position(_pipe_path_curve.point_count - 2).angle_to_point(_pipe_path_curve.get_point_position(_pipe_path_curve.point_count - 1))) * _player.dash_end_velocity_multiplier * _pipe_path_curve.get_baked_length()
 	_player.velocity = new_player_velocity
 	# Enable player input
 	_player.enable_player_input()
