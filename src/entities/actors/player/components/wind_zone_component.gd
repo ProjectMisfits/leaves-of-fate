d2b96b2f227841137d674fc20bb1398aad9d566e
JacobAntepli <70148209+JacobAntepli@users.noreply.github.com>
2026-02-19 08:55:41 -0500

    Commented Out Things print statement

--- src/entities/actors/player/components/wind_zone_component.gd
+++ src/entities/actors/player/components/wind_zone_component.gd
@@ -5,16 +5,16 @@ extends Node2D
 
 func _on_dash_detection_area_body_entered(_body: Node2D) -> void:
 	player._current_leaf_dash_mode = player.leaf_dash_mode.DASH_ONLY
-	print("WEEEEEEEEE`")
+	#print("WEEEEEEEEE`")
 	pass # Replace with function body.
 
 
 func _on_no_dash_detection_area_body_entered(_body: Node2D) -> void:
 	player._current_leaf_dash_mode = player.leaf_dash_mode.NO_DASH
-	print("NO DASH BABY")
+	#print("NO DASH BABY")
 	pass # Replace with function body.
 
 func _on_reset_zone_area_body_entered(_body: Node2D) -> void:
 	player._current_leaf_dash_mode = player.leaf_dash_mode.NORMAL
-	print("ResetBaby")
+	#print("ResetBaby")
 	pass # Replace with function body.
