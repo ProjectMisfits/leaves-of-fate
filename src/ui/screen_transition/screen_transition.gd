d966a05cec48d6fefdbc4575e7457aefb6c7a4c1
Jeremy King <jeremykingdev@gmail.com>
2026-02-23 14:45:31 -0500

    make screen transition handle the circle shader only if the transition is circle

--- src/ui/screen_transition/screen_transition.gd
+++ src/ui/screen_transition/screen_transition.gd
@@ -20,7 +20,6 @@ func start_transition(transition_type: String, pos: Vector2 = Vector2.INF) -> vo
 	if not transition_animation_player.has_animation(transition_type):
 		push_error("ScreenTransition: Transition type '%s' not found." % transition_type)
 		return
-	
 	# Set the screen transition type for use in the transition out animation.
 	screen_transition_type = transition_type
 	# Update the position for use by the animation.
@@ -34,27 +33,29 @@ func finish_transition(pos: Vector2 = Vector2.INF) -> void:
 	if not screen_transition_type:
 		push_error("ScreenTransition: No screen transition type set or screen transition in progress.")
 		return
-	
 	# Update the position for use by the animation.
 	input_position = pos
 	# Play the animation and wait for it to finish.
 	transition_animation_player.play_backwards(screen_transition_type)
 	await transition_animation_player.animation_finished;
 
-## Handle the circle transition animation.
+## Handle the circle transition animation's iris position.
 func _circle_transition() -> void:
-	pass
-
-## Sets up the the circle wipe positioning uniforms from the value of input_position.
-func _circle_wipe_set_position() -> void:
-	var mat: ShaderMaterial = $CanvasLayer/ColorRect.material
+	# Only let this run at the correct times based on the animation.
+	if not $CanvasLayer/ColorRect.is_inside_tree():
+		return
+	
+	# Set the ColorRect's shader material.
+	var transition_shape: ColorRect = $CanvasLayer/ColorRect
+	var transition_shader_material: ShaderMaterial = preload("res://src/ui/screen_transition/circle_transition_shader_material.tres")
+	transition_shape.material = transition_shader_material
 	if input_position == Vector2.INF:
 		# If the irising position is not set, default to using the center of the screen for the transition.
-		mat.set_shader_parameter("useCenterPosition", false)
+		transition_shape.material.set_shader_parameter("useCenterPosition", false)
 	else:
 		# Otherwise iris on the set position.
-		mat.set_shader_parameter("useCenterPosition", true)
-		mat.set_shader_parameter("centerPosition", get_canvas_transform() * input_position)
+		transition_shape.material.set_shader_parameter("useCenterPosition", true)
+		transition_shape.material.set_shader_parameter("centerPosition", get_canvas_transform() * input_position)
 
 ## Handle the fade transition animation.
 func _fade_transition() -> void:
