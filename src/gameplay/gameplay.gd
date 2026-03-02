895a0319a90e9d39d5d7c066fb7d048fb749793b
GabrielTayl0r <71901831+GabrielTayl0r@users.noreply.github.com>
2026-03-02 13:57:14 -0500

    Added comments

--- src/gameplay/gameplay.gd
+++ src/gameplay/gameplay.gd
@@ -53,6 +53,7 @@ func _first_room_setup() -> void:
 		# Put the player in the first room.
 		SceneManager.swap_scenes(EventFlags.first_room_path, room_holder, null)
 		current_room = room_holder.get_child(0)
+		# Sets the current room as the first room
 		current_room_path = EventFlags.first_room_path
 		_init_room(current_room.get_door_position('enter'))
 		player_spawn_location = current_room.get_door_position('enter')
--- src/ui/main_menu/main_menu.gd
+++ src/ui/main_menu/main_menu.gd
@@ -28,16 +28,21 @@ func _process(_delta: float) -> void:
 func _on_play_button_button_up() -> void:
 	select_audio.play()
 	await select_audio.finished
+	#Creates new save files
 	EventFlags.reset_all_flags()
 	SaveManager._save_room("res://src/rooms/01_great_hall/01_GreatHall_a_Intro_room.tscn")
 	SaveManager._save_flags()
+	#Loads intro letter
 	SceneManager.swap_scenes_with_transition("res://src/ui/intro_letter/intro_letter.tscn", null, self)
-	
+
+# Signals that the game is started.
 func _on_continue_button_button_up() -> void:
 	select_audio.play()
 	await select_audio.finished
+	#Loads data from save files
 	SaveManager._load_flags()
 	SaveManager._load_room()
+	#Loads room saved in save file
 	SceneManager.swap_scenes_with_transition("res://src/gameplay/gameplay.tscn", null, self)
 
 func _hide_main_menu() -> void:
