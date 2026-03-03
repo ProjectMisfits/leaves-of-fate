1908e39b66bb4ebf3e3a5514dd6fe10949cefbe0
Jeremy King <jeremykingdev@gmail.com>
2026-03-02 21:37:14 -0500

    add logic to allow player enter bronze pipe from overlapping

--- src/entities/triggers/player_enter_trigger/bronze_pipe_trigger/bronze_pipe_trigger.gd
+++ src/entities/triggers/player_enter_trigger/bronze_pipe_trigger/bronze_pipe_trigger.gd
@@ -15,3 +15,10 @@ func _enter_pipe() -> void:
 	if player.state_machine.get_active_state() == player.dashing_state:
 		# If so, emit a pipe entered signal with the player for the pipe to catch
 		pipe_entered.emit(player)
+
+func _physics_process(_delta: float) -> void:
+	var bodies: Array[Node2D] = get_overlapping_bodies()
+	for body: Node2D in bodies:
+		if body is Player:
+			if player.state_machine.get_active_state() == player.dashing_state:
+				_enter_pipe()
